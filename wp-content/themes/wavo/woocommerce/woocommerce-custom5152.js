722e5d6b4dc92795e55f02b24920e76c6be0cde4
Sohag Hasan <sohag.web.dev@gmail.com>
2020-11-24 23:26:37 +0600

    init

---
+++ wp-content/themes/wavo/woocommerce/woocommerce-custom5152.js
@@ -0,0 +1,3 @@
+(function($){'use strict';function _wrapRelatedTitle(){var text=$('.related.products > h2').text();$('.related.products > h2').addClass('__title');var split=text.split(' ');var firstLetter=split[0];var rest=split.splice(1,split.length)
+$('.related.products > h2').html('<span>'+firstLetter+'</span> '+rest.join(''));$('.related.products > h2').wrap('<div class="section-heading"></div>');}
+$(document).ready(function(){_wrapRelatedTitle();});}(jQuery));
